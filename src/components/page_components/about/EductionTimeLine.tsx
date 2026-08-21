6458a4b46222cce04c8c7313276efdb7f10400dc
Charles <charles.degraeuwe@student.hogent.be>
2026-08-21 02:38:38 +0200

    fix: general fixes (#8)

--- src/components/page_components/about/EductionTimeLine.tsx
+++ src/components/page_components/about/EductionTimeLine.tsx
@@ -1,47 +1,55 @@
 import classNames from "../../../util/classNames.ts";
 
-//TODO vervangen door md file voor template
 const OPLEIDING = [
     {
-        datum: 2006,
+        startDate: 2006,
+        endDate: 2006,
         title: "born in Ghent",
         description: "Born & raised in Ghent, Belgium",
-        active: false
     },
     {
-        datum: "2024 - 2027",
+        startDate: 2024,
+        endDate: 2027,
         title: "Bachelor in applied Computer Science",
         description: "HOGENT, \nfocused on software engineering and AI",
-        active: true
     },
     {
-        datum: "2027 - 2029",
+        startDate: 2027,
+        endDate: 2029,
         title: "Bridge years for mathematics & science",
         description: "UGent, \nbridging program to transition into an engineering degree",
-        active: false
     },
     {
-        datum: "2029 - 2031",
+        startDate: 2029,
+        endDate: 2031,
         title: "Master in Engineering: Computer Science",
         description: "UGent, \nspecializing in software engineering and systems design",
-        active: false
     },
 ]
 const EductionTimeLine = () => {
-    return(
+    const currentYear = new Date().getFullYear();
+    return (
         <div className={"flex ml-10 mb-10 py-5 h-fit flex-col gap-15 border-l border-zinc-300"}>
-            {OPLEIDING.map(item => (
-                <div className={'flex flex-col px-10 relative gap-3'}>
-                    <div className={'justify-center flex flex-col h-fit gap-3'}>
-                        <div className={classNames('absolute -left-1.75 rounded-full border bg-white border-zinc-500 w-3 min-w-3 min-h-3 h-3', item.active && "border-amber-600 bg-amber-100")}/>
-                        <div className={"text-zinc-500/85"}><span className={"font-medium text-zinc-900"}>{item.datum}</span> - {item.title}</div>
-                    </div>
-                    {item.description && (<p className={"max-w-100 whitespace-pre-line text-sm text-zinc-400"}>
-                        {item.description}
-                    </p>)}
+            {OPLEIDING.map(item => {
+                const hasPassed = item.endDate < currentYear;
+                const isActive = item.endDate > currentYear && item.startDate < currentYear;
+                return (
+                    <div className={'flex flex-col px-10 relative gap-3'}>
+                        <div className={'justify-center flex flex-col h-fit gap-3'}>
+                            <div
+                                className={classNames('absolute -left-1.75 rounded-full border bg-white border-zinc-500 w-3 min-w-3 min-h-3 h-3',
+                                    isActive && "border-amber-600 bg-amber-100",
+                                )}/>
+                            <div className={classNames("text-zinc-500/85",  !hasPassed && !isActive && "opacity-35")}><span
+                                className={"font-medium text-zinc-900"}>{item.startDate === item.endDate ? item.startDate : item.startDate + " - " + item.endDate}</span> - {item.title}</div>
+                        </div>
+                        {item.description && (<p className={classNames("max-w-100 whitespace-pre-line text-sm text-zinc-400", !hasPassed && !isActive && "opacity-35")}>
+                            {item.description}
+                        </p>)}
 
-                </div>
-            ))}
+                    </div>
+                )
+            })}
 
         </div>
     )
